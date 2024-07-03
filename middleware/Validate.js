a371f291155f1344d7bf3d5cbce98f8dfe7321d4
unknown <rutwik2514@gmail.com>
2024-07-03 11:12:45 +0530

    sell controllers modified

--- middleware/Validate.js
+++ middleware/Validate.js
@@ -1,7 +1,26 @@
-import express from 'express'
 
-let regexemail= /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
 
-export const validemail=(value)=>{
- return  regexemail.test(value);
-}
+
+
+
+
+import express from "express";
+import jwt from "jsonwebtoken";
+
+export const checkAuthorization = async (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (token == null || !token || token == undefined) {
+    res.status(403).send({ message: "Something went wrong" });
+    return;
+  }
+  const request_token = jwt.verify(token, process.env.JWTSECRET);
+  req.decoded_token = request_token;
+  // console.log("jijijijijddd");
+  // console.log(token);
+  if (!request_token) {
+    res.status(403).send({ message: "Something went wrong" });
+    return;
+  }
+  next();
+};
--- middleware/VerifyUser.js
+++ middleware/VerifyUser.js
@@ -1,20 +1,7 @@
-import express from "express";
-import jwt from "jsonwebtoken";
+import express from 'express'
 
-export const checkAuthorization = async (req, res, next) => {
-  const token = req.headers["authorization"];
+let regexemail= /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
 
-  if (token == null || !token || token == undefined) {
-    res.status(403).send({ message: "Something went wrong" });
-    return;
-  }
-  const request_token = jwt.verify(token, process.env.JWTSECRET);
-  req.decoded_token = request_token;
-  // console.log("jijijijijddd");
-  // console.log(token);
-  if (!request_token) {
-    res.status(403).send({ message: "Something went wrong" });
-    return;
-  }
-  next();
-};
+export const validemail=(value)=>{
+ return  regexemail.test(value);
+}
